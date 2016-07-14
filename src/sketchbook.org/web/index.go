dfea6bccb7d0041fcd5c040a2b5cac9af64cf121
baeks <baeks@AYME>
2016-07-14 17:29:49 +0900

    aaa

--- src/sketchbook.org/web/index.go
+++ src/sketchbook.org/web/index.go
@@ -3,7 +3,7 @@ package web
 import ()
 
 func loadIndexContents() (*indexPage, error) {
-	return &indexPage{Title: "SketchBook", Body: []byte("First GO Language Project")}, nil
+	return &indexPage{Title: "CafeLivro", Body: []byte("First GO Language Project")}, nil
 }
 
 type indexPage struct {
