34336f176d9f6a52bb4efca01c45dff3aabfa149
SEUNGWOO BAEK <SEUNGWOO BAEK>
2016-07-14 20:00:39 +0900

    profile

--- src/main/cafelivro.go
+++ src/main/cafelivro.go
@@ -13,6 +13,7 @@ func main() {
 	//example.Example3()
 	port := os.Getenv("PORT")
 	println("port : ", port)
+	println("port : ", port)
 	http.Handle("/css/", new(web.StaticHandler))
 	http.Handle("/fonts/", new(web.StaticHandler))
 	http.Handle("/js/", new(web.StaticHandler))
