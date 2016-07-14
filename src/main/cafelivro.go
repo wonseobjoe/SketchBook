9cd1e03c123184149627704f50a675a2c6b045e9
SEUNGWOO BAEK <SEUNGWOO BAEK>
2016-07-14 19:52:02 +0900

    profile

--- src/main/cafelivro.go
+++ src/main/cafelivro.go
@@ -3,6 +3,7 @@ package main
 import (
 	//"sketchbook.org/web/example"
 	"net/http"
+	"os"
 	"sketchbook.org/web"
 )
 
@@ -10,10 +11,11 @@ func main() {
 	//example.Example1()
 	//example.Example2()
 	//example.Example3()
-
+	port := os.Getenv("PORT")
+	println("port : ", port)
 	http.Handle("/css/", new(web.StaticHandler))
 	http.Handle("/fonts/", new(web.StaticHandler))
 	http.Handle("/js/", new(web.StaticHandler))
 	http.HandleFunc("/", web.IndexHandler)
-	http.ListenAndServe(":7070", nil)
+	http.ListenAndServe(":"+port, nil)
 }
