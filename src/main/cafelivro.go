ab2f07a367c02b3f3cef0c5dd2e6af2b7eec3fcc
baeks <baeks@daniela.local>
2016-07-19 00:47:26 +0900

    Signed-off-by: baeks <baeks@daniela.local>

--- src/main/cafelivro.go
+++ src/main/cafelivro.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-<<<<<<< HEAD
-	//"cafelivro/httpclient"
 	"cafelivro/web"
 	"net/http"
 	"os"
@@ -19,26 +17,5 @@ func main() {
 	http.HandleFunc("/", web.IndexHandler)
 	http.HandleFunc("/news/", web.NewsHandler)
 	http.ListenAndServe(":"+port, nil)
-	//httpclient.Execute()
-=======
-	//	"cafelivro/web"
-	//	"net/http"
-	//	"os"
-	"cafelivro/httpclient"
-)
 
-func main() {
-	httpclient.Execute()
-	/*
-		port := os.Getenv("PORT")
-		//port = "80"
-
-		http.Handle("/css/", new(web.StaticHandler))
-		http.Handle("/fonts/", new(web.StaticHandler))
-		http.Handle("/js/", new(web.StaticHandler))
-		http.HandleFunc("/", web.IndexHandler)
-		http.ListenAndServe(":"+port, nil)
-	*/
-
->>>>>>> 25bdae1bef2acaf5545b61c22e3326e2cf25be93
 }
